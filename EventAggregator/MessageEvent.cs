6d39f4ed6ed435816784ad38e337e962c2ec5b1a
ian <ardianto.nugroho1994@gmail.com>
2021-08-18 09:30:24 +0700

    References AP-7 event message instance
    feat: add event aggregator instance
    References AP-7 event message instance

---
+++ EventAggregator/MessageEvent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Easy.MessageHub;
+
+namespace EventAggregator
+{
+    public class MessageEvent
+    {
+        private readonly IMessageHub _messageHub;
+
+        public MessageEvent()
+        {
+            //adding comment
+            _messageHub = new MessageHub();
+            _messageHub.RegisterGlobalHandler((type, eventObject) => Console.WriteLine($"Type: {type} - Event: {eventObject}"));
+        }
+
+        private static MessageEvent _masterMessage;
+        public static MessageEvent MakeInstance()
+        {
+            _masterMessage = new MessageEvent();
+            return _masterMessage;
+        }
+
+        public static MessageEvent GetInstance()
+        {
+            return _masterMessage;
+        }
+
+        public void Publish<T>(T message)
+        {
+            _messageHub.Publish(message);
+        }
+
+        public Guid Subscribe<T>(System.Action<T> action)
+        {
+            return _messageHub.Subscribe(action);
+        }
+
+        public void UnSubscribe(Guid token)
+        {
+            _messageHub.Unsubscribe(token);
+        }
+    }
+}
